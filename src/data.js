d77d036862ad36f449e8159fd5e9a504ec786ab8
nesnip <hpmarines12@gmail.com>
2019-10-29 00:50:37 -0500

    Exporta descripción de personajes en otro card

--- src/data.js
+++ src/data.js
@@ -4,14 +4,45 @@
 
 export const showPotterCharacters = (data) => {
   let templatePotter = '';
-  data.map(obj => {
+  data.map((obj) => {
     templatePotter += `
-    <div class="card">
-        <img src = '${obj.image}' alt = "Image" class="photoCharacter"/>
+    <div class="card hide" id="cardCharacter">
+        <img src="${obj.image}" alt="Image" class="photoCharacter"/>
       <div class="container">
         <p>${obj.name}</p>
       </div>
     </div>`;
   });
   return templatePotter;
+};
+
+/* export const showPotterCharacters = (data) => {
+  return data
+    .map((obj) => (`
+      <div class="card">
+          <img src="${obj.image}" alt="Image" class="photoCharacter"/>
+        <div class="container">
+          <p>${obj.name}</p>
+        </div>
+      </div>`
+    ))
+    .join('');
+}; */
+
+export const showPotterDescription = (data2) => {
+  let templatePotter2 = '';
+  data2.map((obj) => {
+    templatePotter2 += `
+    <div class="card hide" id="cardDescription">
+      <div class="container">
+        <h2>${obj.name}</h2>
+        <p>Especie: ${obj.species}</p>
+        <p>Status de sangre: ${obj.ancestry}</p>
+        <p>Casa: ${obj.house}</p>
+        <p>Patronus: ${obj.patronus}</p>
+        <p>Actor: ${obj.actor}</p>
+      </div>
+    </div>`;
+  });
+  return templatePotter2;
 };
