cc1b9b514798f81e8cc08fd31f8d058fdad63467
nesnip <hpmarines12@gmail.com>
2019-10-29 12:30:03 -0500

    Se corrigen issues

--- src/main.js
+++ src/main.js
@@ -7,23 +7,12 @@ import POTTER from './data/potter/potter.js';
 
 console.log(POTTER);
 
-// import {showPotterCharacters} from './data.js';
-import {showPotterDescription} from './data.js';
+import {showPotterCharacters} from './data.js';
 
-// document.querySelector('#insertPotterCharacter').innerHTML = showPotterCharacters(POTTER);
-document.querySelector('#insertPotterDescription').innerHTML = showPotterDescription(POTTER);
+document.querySelector('#insertPotterCharacter').innerHTML = showPotterCharacters(POTTER);
 
-const photoCharacter = document.querySelector('.photoCharacter');
-const cardCharacter = document.querySelector('#cardCharacter');
-const cardDescription = document.querySelector('#cardDescription');
-cardDescription.classList.add('hide');
-cardCharacter.classList.remove('hide');
-photoCharacter.addEventListener('click', () => {
-  cardCharacter.classList.add('hide');
-  cardDescription.classList.remove('hide');
-});
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+const AZ = document.querySelector('#A-Z');
+
+AZ.addEventListener('click', () => {
+  
+})
