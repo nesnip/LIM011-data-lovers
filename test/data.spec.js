5217f9df59db56a946b48ea72cdee99718c21da5
andeluci <andeluci04@gmail.com>
2019-11-11 11:20:23 -0500

    Se realiza modificaciones segun slint a data.js y data.spec.js

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,15 +1,14 @@
 // importamos la función `example`
-import { orderAscDescByName } from "../src/data";
+import { orderAscDescByName } from '../src/data';
 
 describe('orderAscDescByName', () => {
-
   it('debería ser una función', () => {
     expect(typeof orderAscDescByName).toBe('function');
   });
 
   describe('Es una funcion que ordena Ascendente y Descendente', () => {
-    const input = [{name: 'Harry Potter'}, {name: 'Hermione Granger'}, {name: 'Ron Weasley'}, {name: 'Draco Malfoy'}];
-    const output = [{name: 'Draco Malfoy'}, {name: 'Harry Potter'}, {name: 'Hermione Granger'}, {name: 'Ron Weasley'}];
+    const input = [{ name: 'Harry Potter' }, { name: 'Hermione Granger' }, { name: 'Ron Weasley' }, { name: 'Draco Malfoy' }];
+    const output = [{ name: 'Draco Malfoy' }, { name: 'Harry Potter' }, { name: 'Hermione Granger' }, { name: 'Ron Weasley' }];
     expect(orderAscDescByName(input)).toEqual(output, output.reverse());
   });
 });
